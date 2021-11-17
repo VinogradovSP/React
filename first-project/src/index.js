56fb7b25dd192f329b7c990d7091eecf9732f911
Vin.S <gift121279@gmail.com>
2021-11-17 21:47:00 +0300

    update to lesson_2

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,22 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const title = "Hello friend";
-
-const Message = ({ text }) => {
+const Message = ({ messageList }) => {
   return (
-    <h3>
-      I say: <span className="span">{text}</span>
-    </h3>
+    <div className="messageDiv">
+      {messageList.map((message) => (
+        <div className="messageBlock">
+          <div>
+            <b>{message.author.toUpperCase()}:</b> {message.text}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
 const App = () => {
+  const [messageList, setMessageList] = useState([]);
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const lastMessage = messageList[messageList.length - 1];
+    let timerId = null;
+
+    if (messageList.length && lastMessage.author !== "Bot") {
+      setTimeout(() => {
+        setMessageList([
+          ...messageList,
+          { author: "Bot", text: `Hello ${lastMessage.author.toUpperCase()}` },
+        ]);
+      }, 1500);
+    }
+    return () => clearInterval(timerId);
+  }, [messageList]);
+
+  const addMessage = () => {
+    setMessageList([...messageList, { author: author, text: text }]);
+    setAuthor("");
+    setText("");
+  };
+
   return (
-    <div className="head">
-      <h1>Hello React</h1>
-      <Message text={title} />
+    <div className="contaner">
+      <div className="head">
+        <Message messageList={messageList} />
+        <div className="messageFutter">
+          <input
+            placeholder="Введите ваше имя"
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+            value={author}
+          />
+
+          <input
+            placeholder="Введите сообщение"
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+            value={text}
+          />
+          <button onClick={addMessage}>addMessage</button>
+        </div>
+      </div>
     </div>
   );
 };
